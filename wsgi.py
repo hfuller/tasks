4be23d00b2bb3251678cd07f4e85eb77a3c931a6
Hunter Fuller <hfuller@pixilic.com>
2019-04-22 22:27:02 -0500

    Add "my first app", basically, that lists out the tasks.

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import psycopg2, psycopg2.extras
+import pprint
+
+app = Flask(__name__)
+
+print("Opening DB")
+conn = psycopg2.connect(dbname="tasks", cursor_factory=psycopg2.extras.RealDictCursor)
+conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+print(conn)
+
+@app.route('/')
+def list_tasks_html():
+    cursor = conn.cursor()
+    cursor.execute("select * from task")
+    #return pprint.pformat(cursor.fetchall())
+    return render_template('list.j2', tasks=cursor.fetchall())
